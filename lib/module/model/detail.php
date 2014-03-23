16a9fbb9f6f46c8045b81e2c798909f643b58c67
just-paja <just.paja@gmail.com>
2014-03-23 12:34:56 +0100

    Detail, use partial rendering

--- lib/module/model/detail.php
+++ lib/module/model/detail.php
@@ -6,8 +6,26 @@ $this->req('model');
 def($conds, array());
 def($opts, array());
 
+$response = array(
+	'status'  => 404,
+	'message' => 'model-not-found'
+);
+
 $model = System\Loader::get_class_from_model($model);
 
-if ($item = find($model, $id)) {
-	$this->json_response(200, $item->to_object());
-} else $this->json_response(404, 'Object was not found');
+if (class_exists($model)) {
+	if ($item = find($model, $id)) {
+		if ($item->can_be(\System\Model\Perm::VIEW, $request->user)) {
+			$response['status']  = 200;
+			$response['message'] = 'ok';
+			$response['data']    = $item->to_object();
+		} else {
+			$response['status'] = 403;
+			$response['message'] = 'access-denied';
+		}
+	} else {
+		$response['message'] = 'object-not-found';
+	}
+}
+
+$this->partial(null, $response);
