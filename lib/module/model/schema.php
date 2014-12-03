f36468fb418d236a03f72b95effe12bb76690bd2
just-paja <just.paja@gmail.com>
2014-12-03 16:30:30 +0100

    Cache model schema

--- lib/module/model/schema.php
+++ lib/module/model/schema.php
@@ -5,7 +5,7 @@ $this->req('model');
 $cname = System\Loader::get_class_from_model($model);
 $exists = class_exists($cname) && is_subclass_of($cname, '\System\Model\Perm');
 
-$response = array(
+$res = array(
 	'status'  => 404,
 	'message' => 'schema-not-found'
 );
@@ -91,14 +91,30 @@ if ($exists) {
 				}
 			}
 
-			$response['status'] = 200;
-			$response['message'] = 'ok';
-			$response['data'] = $attrs;
+			$res['status'] = 200;
+			$res['message'] = 'ok';
+			$res['data'] = $attrs;
 		}
 	} else {
-		$response['status'] = 403;
-		$response['message'] = 'access-denied';
+		$res['status'] = 403;
+		$res['message'] = 'access-denied';
 	}
 }
 
-$this->partial(null, $response);
+
+try {
+	$debug = \System\Settings::get('dev', 'debug', 'backend');
+} catch(\System\Error $e) {
+	$debug = true;
+}
+
+if (!$debug) {
+	$max_age = \System\Settings::get('cache', 'resource', 'max-age');
+
+	$response->header('Pragma', 'public,max-age='.$max_age);
+	$response->header('Cache-Control', 'public');
+	$response->header('Expires', date(\DateTime::RFC1123, time() + $max_age + rand(0,60)));
+	$response->header('Age', '0');
+}
+
+$this->partial(null, $res);
