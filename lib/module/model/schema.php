34f2e1f6c1f57a7e8f3587b7109eaf905f54aa8e
just-paja <just.paja@gmail.com>
2014-12-10 19:32:24 +0100

    Schema module, use fudjan native methods

--- lib/module/model/schema.php
+++ lib/module/model/schema.php
@@ -17,84 +17,11 @@ $rel_attrs = array(
 
 if ($exists) {
 	if ($cname::can_user(\System\Model\Perm::VIEW_SCHEMA, $request->user)) {
-		$menu = Godmode\Router::get_schemas();
+		$schema = $cname::get_visible_schema($request->user);
 
-		if (true || in_array($model, $menu)) {
-			$attrs = array();
-			new $cname();
-			$def = \System\Model\Database::get_model_attr_list($cname, false, true);
-
-			foreach ($def as $name) {
-				if ($name != \System\Model\Database::get_id_col($cname)) {
-					$attr = \System\Model\Database::get_attr($cname, $name);
-					$attr['name'] = $name;
-
-					if (in_array($attr[0], $rel_attrs)) {
-						$rel_cname = $attr['model'];
-						$is_subclass = is_subclass_of($rel_cname, '\System\Model\Perm');
-
-						if (!($is_subclass && $rel_cname::can_user(\System\Model\Perm::VIEW_SCHEMA, $request->user))) {
-							$attr = null;
-							continue;
-						}
-					}
-
-					switch ($attr[0])
-					{
-						case 'bool': $attr['type'] = 'boolean'; break;
-						case 'varchar': $attr['type'] = 'string'; break;
-						case 'json': $attr['type'] = 'object'; break;
-						case \System\Model\Database::REL_BELONGS_TO: $attr['type'] = 'model'; break;
-						case \System\Model\Database::REL_HAS_MANY: $attr['type'] = 'collection'; break;
-						default: $attr['type'] = $attr[0];
-					}
-
-					if (isset($attr['model'])) {
-						$attr['model'] = \System\Loader::get_model_from_class($rel_cname);
-					}
-
-					if (isset($attr['options'])) {
-						if (isset($attr['options'][0]) && $attr['options'][0] == 'callback') {
-							$opts = $attr['options'];
-							array_shift($opts);
-							$opts = call_user_func($opts);
-						} else {
-							$opts = $attr['options'];
-						}
-
-						$attr['options'] = array();
-
-						foreach ($opts as $opt_value=>$opt_name) {
-							$attr['options'][] = array(
-								'name'  => $opt_name,
-								'value' => $opt_value,
-							);
-						}
-					}
-
-					// Word 'default' is keyword in some browsers, so pwf-models use 'def' instead
-					if (isset($attr['default'])) {
-						$attr['def'] = $attr['default'];
-						unset($attr['default']);
-
-						if (in_array($attr[0], array('image'))) {
-							$attr['def'] = \System\Image::from_path($attr['def'])->to_object();
-						} else if (in_array($attr[0], array('file', 'sound'))) {
-							$attr['def'] = \System\File::from_path($attr['def'])->to_object();
-						}
-					}
-
-					if (is_array($attr)) {
-						unset($attr[0]);
-						$attrs[] = $attr;
-					}
-				}
-			}
-
-			$res['status'] = 200;
-			$res['message'] = 'ok';
-			$res['data'] = $attrs;
-		}
+		$res['status'] = 200;
+		$res['message'] = 'ok';
+		$res['data'] = $schema;
 	} else {
 		$res['status'] = 403;
 		$res['message'] = 'access-denied';
