4f499213bf85385f61eaab082d2cc7cd374ba0ca
just-paja <just.paja@gmail.com>
2015-03-30 13:15:55 +0200

    Resource like schema pack

---
+++ lib/class/system/resource/schema.php
@@ -0,0 +1,56 @@
+<?
+
+namespace System\Resource
+{
+	class Schema extends \System\Resource\Json
+	{
+		public function resolve()
+		{
+			$this->exists = true;
+
+			try {
+				$packages = cfg('resources', 'models', $this->name);
+			} catch (\System\Error\Config $e) {
+				$this->exists = false;
+			}
+		}
+
+
+		public function read()
+		{
+			$list = cfg('resources', 'models', $this->name);
+			$rels = array('collection', 'model');
+			$data = array();
+			$pack = array();
+
+			foreach ($list as $model) {
+				$cname  = \System\Loader::get_class_from_model($model);
+				$model  = \System\Loader::get_model_from_class($model);
+				$schema = $cname::get_visible_schema($this->request->user);
+
+				foreach ($schema['attrs'] as $attr) {
+					if (in_array($attr['type'], $rels)) {
+						$rel_cname = \System\Loader::get_class_from_model($attr['model']);
+						$rel_model = \System\Loader::get_model_from_class($attr['model']);
+
+						if (!array_key_exists($rel_model, $pack)) {
+							$pack[$rel_model] = $rel_cname::get_visible_schema($this->request->user);
+						}
+					}
+				}
+
+				$pack[$model] = $schema;
+			}
+
+			foreach ($pack as $name => $def) {
+				$data[] = array(
+					"name"    => $name,
+					"parents" => array('model'),
+					"static"  => $def
+				);
+			}
+
+			$this->content = json_encode(array("data" => $data));
+		}
+	}
+}
--- lib/policy/frontend_api.php
+++ lib/policy/frontend_api.php
@@ -5,8 +5,11 @@ $policy = function($rq, $res) {
 		return \System\Router::get_route_str($rq->host, $name);
 	};
 
+	$url_pack = $past_route($rq, 'system_resource');
+	$url_pack = str_replace(array('{res_src}', '{res_type}', '{res_path}'), array('media', 'schema', '{name}.'.\System\Resource::get_serial()), $url_pack);
+
 	$urls = array(
-		'pack'   => $past_route($rq, 'api_schema'),
+		'pack'   => $url_pack,
 		'schema' => $past_route($rq, 'api_model_schema'),
 		'browse' => $past_route($rq, 'api_model_browse'),
 		'create' => $past_route($rq, 'api_model_create'),
