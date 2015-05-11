b51b835506514b680ce2c5eb44190f82a1be9b00
just-paja <just.paja@gmail.com>
2015-05-11 13:43:09 +0200

    Coding style, more readable API browser

--- lib/module/api/model/browse.php
+++ lib/module/api/model/browse.php
@@ -4,301 +4,316 @@ namespace Module\Api\Model
 {
 	class Browse extends \System\Module
 	{
-		public function run()
+		protected $status = 200;
+		protected $joins = array();
+		protected $cname;
+		protected $query;
+		protected $count;
+		protected $sort = array();
+		protected $filters = array();
+		protected $items = array();
+		protected $result = array();
+
+
+		public function request_decode()
 		{
-			def($conds, array());
-			def($opts, array());
+			$this->cname = \System\Loader::get_class_from_model($this->req('model'));
+			$this->request_decode_pagination();
 
-			$model = $this->req('model');
-			$page = $this->req('page');
-			$rq = $this->request;
+			$this->filters = $this->request_decode_part('filters');
+			$this->sort    = $this->request_decode_part('sort');
+			$this->joins   = $this->request_decode_part('join');
+		}
 
-			$response = array(
-				'status'  => 200,
-				'message' => null,
-			);
 
-			$cname = \System\Loader::get_class_from_model($model);
-			$status = 200;
-			$meta = null;
+		public function request_decode_pagination()
+		{
+			$this->page = intval($this->req('page'));
+			$pp = $this->request->get('per_page');
 
-			$filters = html_entity_decode($rq->get('filters'));
-			$sort    = html_entity_decode($rq->get('sort'));
-			$joins   = html_entity_decode($rq->get('join'));
+			if ($pp) {
+				$this->per_page = intval($pp);
+			}
+		}
 
-			$sort_by = array();
 
-			if ($rq->get('per_page')) {
-				$per_page = intval($rq->get('per_page'));
+		public function request_decode_part($part_name)
+		{
+			$part = html_entity_decode($this->request->get($part_name));
+
+			if ($part) {
+				return \System\Json::decode($part);
 			}
 
-			if (class_exists($cname) && is_subclass_of($cname, '\System\Model\Perm')) {
-				if ($cname::can_user($cname::BROWSE, $rq->user)) {
-					def($sort, array('created_at desc'));
+			return null;
+		}
 
-					$idc = $cname::get_id_col();
 
-					if ($filters) {
-						try {
-							$filters = \System\Json::decode($filters);
-						} catch (\System\Error\Format $e) {
-							$response['status'] = 400;
-							$response['message'] = 'malformed-filter';
-						}
+		public function request_parse_joins()
+		{
+			$cname = $this->cname;
+
+			foreach ($this->joins as $key=>$attr) {
+				if (is_string($attr)) {
+					$attr = array(
+						"attr" => $attr
+					);
+				}
+
+				if (!isset($attr['attr'])) {
+					$this->status = 400;
+					throw new \System\Error\Format('missing-attr-name', $attr);
+				}
+
+				if (!isset($attr['as'])) {
+					$attr['as'] = $attr['attr'];
+				}
+
+				if ($cname::is_rel($attr['attr'])) {
+					$this->joins[$key] = $attr;
+				} else {
+					$this->status = 400;
+					throw new \System\Error\Format('not-a-relation', $attr);
+				}
+			}
+		}
+
+
+		public function request_parse_sort($sort)
+		{
+			if (empty($sort)) {
+				return $this->sort = array('created_at desc');
+			}
+
+			foreach ($sort as $sort_item) {
+				if (is_string($sort_item)) {
+					$sort_tmp  = explode(' ', $sort_item);
+					$sort_item = array('attr' => $sort_tmp[0]);
+
+					if (isset($sort_tmp[1])) {
+						$sort_item['mode'] = $sort_tmp[1];
 					}
+				}
 
-					if ($sort) {
-						try {
-							$sort = \System\Json::decode($sort);
-						} catch (\System\Error\Format $e) {
-							$response['status'] = 400;
-							$response['message'] = 'malformed-sort';
-						}
-					}
+				if (!is_array($sort_item)) {
+					$this->status = 400;
+					throw new \System\Error\Format('malformed-sort', 'must-be-array', $sort_item);
+				}
 
-					if ($joins) {
-						try {
-							$joins = \System\Json::decode($joins);
-						} catch (\System\Error\Format $e) {
-							$response['status'] = 400;
-							$response['message'] = 'malformed-joins';
-						}
-					}
+				if (empty($sort_item['attr'])) {
+					$this->status = 400;
+					throw new \System\Error\Format('malformed-sort', 'missing-key-attr', $sort);
+				}
 
-					if ($response['status'] == 200) {
-						if ($joins) {
-							foreach ($joins as $key=>$attr) {
-								if (is_string($attr)) {
-									$attr = array(
-										"attr" => $attr
-									);
-								}
+				def($sort_item['mode'], 'asc');
 
-								if (!isset($attr['attr'])) {
-									$response['status'] = 400;
-									$response['message'] = 'missing-attr-name';
-									$response['attr'] = $attr;
-									break;
-								}
+				if ($sort_item['attr'] == 'id') {
+					$sort_item['attr'] = $cname::get_id_col($cname);
+				}
 
-								if (!isset($attr['as'])) {
-									$attr['as'] = $attr['attr'];
-								}
+				$sort_by[] = $sort_item['attr'].' '.$sort_item['mode'];
+			}
 
-								if ($cname::is_rel($attr['attr'])) {
-									$joins[$key] = $attr;
-								} else {
-									$response['status'] = 400;
-									$response['message'] = 'not-a-relation';
-									$response['attr'] = $attr;
-								}
-							}
-						}
-					}
-
-					if ($response['status'] == 200) {
-						$query = $cname::get_all($conds, $opts);
-						$data = array();
-
-						if (any($sort)) {
-							foreach ($sort as $sort_item) {
-								if (is_string($sort_item)) {
-									$sort_tmp  = explode(' ', $sort_item);
-									$sort_item = array('attr' => $sort_tmp[0]);
-
-									if (isset($sort_tmp[1])) {
-										$sort_item['mode'] = $sort_tmp[1];
-									}
-								}
-
-								if (is_array($sort_item)) {
-									def($sort_item['mode'], 'asc');
-
-									if (isset($sort_item['attr'])) {
-										if ($sort_item['attr'] == 'id') {
-											$sort_item['attr'] = $cname::get_id_col($cname);
-										}
-
-										$sort_by[] = $sort_item['attr'].' '.$sort_item['mode'];
-									} else {
-										$response['status'] = 400;
-										$response['message'] = 'malformed-sort';
-										$response['attr'] = $sort;
-										break;
-									}
-								} else {
-									$response['status'] = 400;
-									$response['message'] = 'malformed-sort';
-									$response['attr'] = $sort_item;
-									break;
-								}
-							}
-						} else {
-							$sort_by = array('created_at desc');
-						}
-					}
+			return $this->sort = $sort_by;
+		}
 
 
-					if ($response['status'] == 200) {
-						if (any($filters)) {
-							foreach ($filters as $filter=>$filter_val) {
-								if ($filter == 'id') {
-									$filter = $idc;
-								}
+		public function request_parse_filters($filters)
+		{
+			if (empty($filters)) {
+				return $this->filters = array();
+			}
 
-								if (is_array($filter_val)) {
-									if (array_keys($filter_val) !== range(0, count($filter_val) - 1)) {
-										try {
-											$query->add_filter($filter_val);
-										} catch(\System\Error\Argument $e) {
-											$response['status'] = 400;
-											$response['message'] = 'invalid-filter';
-											$response['attr'] = array(
-												'filter' => $filter,
-												'val' => $filter_val
-											);
-											break;
-										}
-									} else {
-										$query->where_in($filter, $filter_val);
-									}
-								} else if (is_numeric($filter) && is_string($filter_val)) {
-									$response['status'] = 400;
-									$response['message'] = 'invalid-filter';
-									$response['attr'] = array(
-										'filter' => $filter,
-										'val' => $filter_val
-									);
-									break;
+			foreach ($filters as $filter=>$filter_val) {
+				if (is_numeric($filter) && is_string($filter_val)) {
+					$this->status = 400;
+					throw new \System\Error\Format('invalid-filter', 'must-be-numeric', $filter, $filter_val);
+				}
+			}
+		}
 
-								} else {
-									if ($cname::has_attr($filter)) {
-										$query->where(array($filter => $filter_val));
-									} else if ($cname::has_filter($filter)) {
-										$cname::filter($query, $filter, $filter_val);
-									} else {
-										$response['status'] = 400;
-										$response['message'] = 'unknown-attr';
-										$response['attr'] = $filter;
-										break;
-									}
-								}
-							}
-						}
-					}
 
-					if ($response['status'] == 200) {
-						if (any($joins)) {
-							foreach ($joins as $j) {
-								$attr = $cname::get_attr($j['attr']);
+		public function request_parse()
+		{
+			$this->request_parse_joins($this->joins);
+			$this->request_parse_sort($this->sort);
+			$this->request_parse_filters($this->filters);
+		}
 
-								if ($attr['type'] == 'has_many' && any($attr['is_bilinear'])) {
-									$join_alias = 't_'.$j['attr'];
-									$table_name = $cname::get_bilinear_table_name($attr);
-									$rel_model = $attr['model'];
-									$using = $cname::get_id_col();
 
-									$query->join($table_name, "USING(".$using.")", $join_alias);
-								}
-							}
-						}
-					}
+		public function request_verify()
+		{
+			$cname = $this->cname;
 
-					if ($response['status'] == 200) {
-						$query->paginate($per_page, $page)->sort_by(implode(', ', $sort_by));
-						$send = true;
+			if (!class_exists($cname)) {
+				$this->status = 404;
+				throw new \System\Error\Argument('bad-model-choice', 'unknown-model', $this->model);
+			}
 
-						try {
-							$items = $query->fetch();
-							$count = $query->count();
-						} catch (\System\Error\Database $e) {
-							$send = false;
+			if (!is_subclass_of($cname, '\System\Model\Perm') || !$cname::can_user($cname::BROWSE, $this->request->user)) {
+				$this->status = 403;
+				throw new \System\Error\Argument('bad-model-choice', 'not-allowed', $this->model);
+			}
+		}
 
-							$response['status'] = 400;
-							$response['message'] = $e->get_explanation();
-						}
 
-						if ($send) {
-							$valid = true;
+		public function query_build()
+		{
+			$cname = $this->cname;
 
-							foreach ($items as $item) {
-								$obj = $item->to_object_with_perms($rq->user);
+			$this->query = $cname::get_all();
 
-								if ($joins) {
-									foreach ($joins as $attr) {
-										$allowed = true;
-										$attr_name = $attr['attr'];
-										$def = $cname::get_attr($attr_name);
+			$this->query_build_joins();
+			$this->query_build_filters();
+			$this->query_build_pagination();
+			$this->query_build_sort();
+		}
 
-										if (in_array($def['type'], array($cname::REL_BELONGS_TO, $cname::REL_HAS_ONE))) {
-											$rel = $item->$attr_name;
 
-											if ($rel) {
-												if (($rel instanceof \System\Model\Perm) && $rel->can_be($cname::BROWSE, $rq->user)) {
-													$obj[$attr['as']] = $rel->to_object();
-												} else {
-													$allowed = false;
-												}
-											}
-										} else if ($def['type'] == $cname::REL_HAS_MANY) {
-											$rel_cname = $def['model'];
+		public function query_build_joins()
+		{
+			$cname = $this->cname;
 
-											if ($rel_cname::can_user($rel_cname::BROWSE, $rq->user)) {
-												$rel = $item->$attr_name;
+			foreach ($this->joins as $j) {
+				$attr = $cname::get_attr($j['attr']);
 
-												if (isset($attr['filters'])) {
-													$rel->add_filters($attr['filters']);
-												}
+				if ($attr['type'] == 'has_many' && any($attr['is_bilinear'])) {
+					$join_alias = 't_'.$j['attr'];
+					$table_name = $cname::get_bilinear_table_name($attr);
+					$rel_model = $attr['model'];
+					$using = $cname::get_id_col();
 
-												if (isset($attr['limit'])) {
-													$rel->paginate($attr['limit']);
-												}
+					$this->query->join($table_name, "USING(".$using.")", $join_alias);
+				}
+			}
+		}
 
-												$rel_data = $rel->fetch();
-												$obj[$attr['as']] = array();
 
-												foreach ($rel_data as $rel_obj) {
-													if ($rel_obj->can_be($cname::BROWSE, $rq->user)) {
-														$obj[$attr['as']][] = $rel_obj->to_object_with_perms($rq->user);
-													}
-												}
-											} else {
-												$allowed = false;
-											}
-										}
+		public function query_build_filters()
+		{
+			$cname = $this->cname;
+			$idc = $cname::get_id_col();
 
-										if (!$allowed) {
-											$valid = false;
-											$response['status'] = 403;
-											$response['message'] = 'access-denied';
-											$response['errors'] = array(
-												array(
-													'rel'     => $attr,
-													'message' => 'denied'
-												)
-											);
-											break;
-										}
-									}
-								}
+			foreach ($this->filters as $filter=>$filter_val) {
+				if ($filter == 'id') {
+					$filter = $idc;
+				}
 
-								$data[] = $obj;
-							}
-
-							if ($valid) {
-								$response['total'] = intval($count);
-								$response['data'] = $data;
-							}
-						}
+				if (is_array($filter_val)) {
+					if (array_keys($filter_val) !== range(0, count($filter_val) - 1)) {
+						$this->query->add_filter($filter_val);
+					} else {
+						$this->query->where_in($filter, $filter_val);
 					}
 				} else {
-					$response['status'] = 403;
-					$response['message'] = 'access-denied';
+					if ($cname::has_attr($filter)) {
+						$this->query->where(array($filter => $filter_val));
+					} else if ($cname::has_filter($filter)) {
+						$cname::filter($query, $filter, $filter_val);
+					} else {
+						$this->status = 400;
+						throw new \System\Error\Format('invalid-filter', 'unknown-attr', $filter);
+					}
 				}
-			} else {
-				$response['status'] = 404;
-				$response['message'] = 'model-not-found';
 			}
+		}
+
+
+		public function query_build_pagination()
+		{
+			$this->query->paginate($this->per_page, $this->page);
+		}
+
+
+		public function query_build_sort()
+		{
+			$this->query->sort_by(implode(',', $this->sort));
+		}
+
+
+		public function query_fetch()
+		{
+			$this->items = $this->query->fetch();
+			$this->count = intval($this->query->count());
+		}
+
+
+		public function result_filter()
+		{
+			$this->result = array();
+
+			foreach ($this->items as $item) {
+				$obj = $item->to_object_with_perms($this->request->user);
+				$this->result_filter_joins($item, $obj);
+				$this->result[] = $obj;
+			}
+		}
+
+
+		public function result_filter_joins($item, &$obj)
+		{
+			$cname = $this->cname;
+
+			foreach ($this->joins as $attr) {
+				$attr_name = $attr['attr'];
+				$def = $cname::get_attr($attr_name);
+
+				if (in_array($def['type'], array($cname::REL_BELONGS_TO, $cname::REL_HAS_ONE))) {
+					$rel = $item->$attr_name;
+
+					if ($rel) {
+						if (!($rel instanceof \System\Model\Perm) || !$rel->can_be($cname::BROWSE, $this->request->user)) {
+							$this->status = 403;
+							throw new \System\Error\Argument('denied-access-to-relation', $attr_name);
+						}
+
+						$obj[$attr['as']] = $rel->to_object();
+					}
+				} else if ($def['type'] == $cname::REL_HAS_MANY) {
+					$rel_cname = $def['model'];
+
+					if (!$rel_cname::can_user($rel_cname::BROWSE, $this->request->user)) {
+						$this->status = 403;
+						throw new \System\Error\Argument('denied-access-to-relation', $attr_name);
+					}
+
+					$rel = $item->$attr_name;
+
+					if (isset($attr['filters'])) {
+						$rel->add_filters($attr['filters']);
+					}
+
+					if (isset($attr['limit'])) {
+						$rel->paginate($attr['limit']);
+					}
+
+					$rel_data = $rel->fetch();
+					$obj[$attr['as']] = array();
+
+					foreach ($rel_data as $rel_obj) {
+						if ($rel_obj->can_be($cname::BROWSE, $this->request->user)) {
+							$obj[$attr['as']][] = $rel_obj->to_object_with_perms($this->request->user);
+						}
+					}
+				}
+			}
+		}
+
+
+		public function run()
+		{
+			$this->request_decode();
+			$this->request_parse();
+			$this->request_verify();
+
+			$this->query_build();
+			$this->query_fetch();
+
+			$this->result_filter();
+
+			$response['status'] = $this->status;
+			$response['total']  = $this->count;
+			$response['data']   = $this->result;
 
 			$this->partial(null, $response);
 		}
